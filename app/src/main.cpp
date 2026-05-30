bf49a5912f8172610856b3e3e2cefb8e389c7480
Gusti Made Arya Wijaya <30534130+Aryaelektro@users.noreply.github.com>
2026-05-30 10:58:57 +0700

    Finish L5-task1 custom board cpy

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,10 +1,9 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
-// #include <zephyr/drivers/pwm.h>
 #include <zephyr/logging/log.h>
 
-#define LED_NODE DT_ALIAS(led0)
+#define LED_NODE DT_ALIAS(app_led)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
@@ -13,6 +12,8 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 int main(void)
 {
     bool led_state = true;
+
+    LOG_INF("Hello World! %s", CONFIG_BOARD);
 
     if (!gpio_is_ready_dt(&led)) return 0;
 
